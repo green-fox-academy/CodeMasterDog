e82e27ac49afd922dc64835d7a4251d0b3c17970
Laszlo Kovacs <kovacslaa@gmail.com>
2017-10-09 18:13:00 +0200

    double task

---
+++ week-02/day-01/double/double.h
@@ -0,0 +1,7 @@
+#define n1 23423
+#define n2 23246778
+#define n3 376864
+#define n4 5476
+#define n5 67
+#define n6 7
+#define n7 87549
---
+++ week-02/day-01/double/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "double.h"
+// create a function called `doubling` that doubles it's input parameter
+// again the parameters value should be stored in a .h file
+
+int main()
+
+{
+    int a = n4;
+    printf("The double of %d is %d\n",a, doubling(a));
+    return 0;
+}
+
+int doubling(var)
+{
+    var=var*2;
+    return var;
+}
