684b2217a7453e2f180bb9e1cd307861ebea66cd
Laszlo Kovacs <kovacslaa@gmail.com>
2017-10-25 12:01:56 +0200

    struct_00.c

---
+++ week-04/day-02/00.c/struct_00.c
@@ -0,0 +1,5 @@
+struct Bitfield bitfield;
+    struct NotBitfield not_bitfield;
+
+    printf("size of Bitfield is: %d bytes.\n", sizeof(bitfield));
+    printf("size of NotBitfield is: %d bytes.\n", sizeof(not_bitfield));
