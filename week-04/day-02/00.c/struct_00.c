af664684ef69c47c09bc981847504f810e7956f4
Laszlo Kovacs <kovacslaa@gmail.com>
2017-10-25 12:59:54 +0200

    struct_00.c fixed, struct_01.c completed

--- week-04/day-02/00.c/struct_00.c
+++ week-04/day-02/00.c/struct_00.c
@@ -1,5 +1,36 @@
-struct Bitfield bitfield;
+#include <stdio.h>
+#include <stdint.h>
+
+struct Bitfield{
+    uint8_t boolean_value_1:1;
+    uint8_t boolean_value_2:1;
+    uint8_t boolean_value_3:1;
+    uint8_t boolean_value_4:1;
+    uint8_t boolean_value_5:1;
+    uint8_t boolean_value_6:1;
+    uint8_t boolean_value_7:1;
+    uint8_t boolean_value_8:1;
+};
+
+struct NotBitfield{
+    uint8_t boolean_value_1;
+    uint8_t boolean_value_2;
+    uint8_t boolean_value_3;
+    uint8_t boolean_value_4;
+    uint8_t boolean_value_5;
+    uint8_t boolean_value_6;
+    uint8_t boolean_value_7;
+    uint8_t boolean_value_8;
+};
+
+int main()
+{
+
+    struct Bitfield bitfield;
     struct NotBitfield not_bitfield;
 
-    printf("size of Bitfield is: %d bytes.\n", sizeof(bitfield));
-    printf("size of NotBitfield is: %d bytes.\n", sizeof(not_bitfield));
+    printf("size of bitfield is: %d\n", sizeof(bitfield));
+    printf("size of notBitfield is: %dn", sizeof(not_bitfield));
+
+    return 0;
+}
