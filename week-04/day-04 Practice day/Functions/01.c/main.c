dbffaa661240568e18fd54a8fbbe536577c1bceb
Laszlo Kovacs <kovacslaa@gmail.com>
2017-10-29 16:08:57 +0100

    practice day: functions 01.c task

---
+++ week-04/day-04 Practice day/Functions/01.c/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+//fill abc string from a to z, print out
+int sum(); //function declaration
+
+int main()
+{
+    printf("The sum is: %d", sum());
+
+    return 0;
+}
+int sum() // function definition
+{
+    int a = 10000;
+    int b = 20000;
+
+    return  a + b;
+}
