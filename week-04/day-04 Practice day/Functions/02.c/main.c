54479e2abf2f3f78514967e56e5fcdc19bd04e34
Laszlo Kovacs <kovacslaa@gmail.com>
2017-10-29 16:14:25 +0100

    practice day: functions 02.c task

---
+++ week-04/day-04 Practice day/Functions/02.c/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+//get two number with scanf
+//calculate their sum with the function
+int sum(int a, int b);
+int main()
+{
+    int first_number = 0;
+    int second_number = 0;
+
+    puts("Please enter two numbers. (separated with space)");
+    scanf("%d %d", &first_number, &second_number);
+    printf("The sum of the two numbers: %d", sum(first_number, second_number));
+
+
+    return 0;
+}
+int sum(int a, int b)
+{
+    //it should return with the sum
+    return a + b;
+}
+
