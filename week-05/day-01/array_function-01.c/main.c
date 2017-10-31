ac36b9b465f5da4d1450379a7a2ad2114d31c230
Laszlo Kovacs <kovacslaa@gmail.com>
2017-10-31 13:48:50 +0100

    array_function-01.c task started

---
+++ week-05/day-01/array_function-01.c/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * array_function-01.c
+ * Create a function that takes an array as argument and it's length. Determine the right data types to do this.
+ * This function should order the arguments in ascending order and return this in a new sorted array.
+ *
+ * Create a second function that takes 3 arguments. An array, it's length and a number it is searching for.
+ * The function should return the index of the number when found or -1 if it's not part of the array.
+ * IMPORTANT: this function expects a sorted array and uses the strategy we used when we play guess the number.
+ *
+ *
+ * In the main function create an unsorted array filled with random numbers, Make it 50 long.
+ * Try 2 scenarios:
+ *    * In the first: You pick a random element in the unosrted array and check at which index it is after the array is sorted.
+ *    * In the second: You pick a number You are sure is not in the array. Check whether the search function returns the right number.
+ * Print out what You need to check these.
+ */
+int main()
+{
+
+    return 0;
+}
