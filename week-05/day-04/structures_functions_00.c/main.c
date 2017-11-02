2aef2b640be9ce7819ca892b262512f291557cd1
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-02 17:17:09 +0100

    structures_functions_00.c create_book function

--- week-05/day-04/structures_functions_00.c/main.c
+++ week-05/day-04/structures_functions_00.c/main.c
@@ -18,16 +18,49 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+int book_counter = 0;
+
+typedef enum {
+    ERROR, SUCCES
+} RETVAL;
 
 typedef struct {
-    char Title[30];
-    char Author[50];
-    int Year_of_pub[3];
+    char title[30];
+    char author[50];
+    int year_of_pub;
 } BOOK_DATA;
+
+//BOOK_DATA create_book_title();
+
+RETVAL create_book(BOOK_DATA *var, char *var_title ,char *var_author ,int var_year)
+{
+
+    if (book_counter > 9)
+        return ERROR;
+
+    strcpy(var[book_counter].title, var_title);
+    strcpy(var[book_counter].author, var_author);
+    var[book_counter].year_of_pub = var_year;
+
+    book_counter++;
+
+
+    return SUCCES;
+}
 
 int main()
 {
-    BOOK_DATA book[4];
+
+
+    BOOK_DATA book[10];
+
+    printf("%d\n", book[0].year_of_pub);
+    RETVAL ret = create_book(book, "Harry Potter", "J. K. R." , 2005);
+    if (ret == SUCCES)
+        printf("%d\n", book[0].year_of_pub);
+    else
+        printf("We can't store more books");
 
 
     return 0;
