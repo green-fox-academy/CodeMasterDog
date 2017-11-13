0244143f8c841804a4c55592dc2ba8e1e40a06a5
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-13 15:24:01 +0100

    rethink of Datatypes/Vector/00.cpp task. c++11 compiler turned off, and fill vector with push_back

--- week-06/day-01/Datatypes/Vector/00.cpp/main.cpp
+++ week-06/day-01/Datatypes/Vector/00.cpp/main.cpp
@@ -5,7 +5,13 @@ using namespace std;
 //create an integer vector with the size of 5 and print the 3rd element of it
 int main()
 {
-    vector<int> my_vector = {77, 69, 5, 4, 6};
+    vector<int> my_vector;
+
+    my_vector.push_back(77);
+    my_vector.push_back(69);
+    my_vector.push_back(5);
+    my_vector.push_back(4);
+    my_vector.push_back(6);
 
     cout << "The vector's 3rd element is: "<<  my_vector[2] ;
 
