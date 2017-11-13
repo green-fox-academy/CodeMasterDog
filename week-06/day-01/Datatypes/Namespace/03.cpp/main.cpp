fa8230417dc7c62fd5e40405d70a2e41172729d5
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-13 14:44:31 +0100

    Datatypes/Namespace/03.cpp task done

--- week-06/day-01/Datatypes/Namespace/03.cpp/main.cpp
+++ week-06/day-01/Datatypes/Namespace/03.cpp/main.cpp
@@ -1,9 +1,26 @@
+// Use your costume namespace from the last exercise
+// Create a function, which takes the age as a parameter
+// and returns the year when you were born
 #include <iostream>
-
 using namespace std;
+
+namespace Person {
+    string name = "Laszlo Kovacs";
+    int age = 35;
+    int fav_num = 1000;
+}
+
+int year_of_born(int);
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    using namespace Person;
+    cout << "I was born in " << year_of_born(age) << endl;
+
     return 0;
+}
+
+int year_of_born(int var)
+{
+    return 2017 - var;
 }
