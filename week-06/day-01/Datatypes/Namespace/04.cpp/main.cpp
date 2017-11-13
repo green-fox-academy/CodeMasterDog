b21f4ea93cca0f9de06bdb216ffffd493c7023d0
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-13 14:53:28 +0100

    Datatypes/Namespace/04.cpp task done

--- week-06/day-01/Datatypes/Namespace/04.cpp/main.cpp
+++ week-06/day-01/Datatypes/Namespace/04.cpp/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 // Create your own namespace, called Mark
 // store your name as a string
 // your age as an integer
@@ -12,8 +10,29 @@
 //
 // create a function that can add the two age, and returns it
 
+#include <iostream>
+using namespace std;
+
+namespace Mark {
+    string name = "Mark";
+    int age = 35;
+    int fav_num = 1000;
+}
+
+namespace Maria {
+    string name = "Maria";
+    int age = 30;
+    int fav_num = 2000;
+}
+
+int add_ages(int, int);
 int main()
 {
-
+    cout << "Sum of the two ages: " << add_ages(Mark :: age, Maria :: age) << endl;
     return 0;
+}
+
+int add_ages(int var1, int var2)
+{
+    return var1 + var2;
 }
