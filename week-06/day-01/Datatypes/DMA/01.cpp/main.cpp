49bfe7ae64674af39354b252a099a5e9ecc7aa5d
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-13 22:37:15 +0100

    Datatypes/DMA/01.cpp task done

--- week-06/day-01/Datatypes/DMA/01.cpp/main.cpp
+++ week-06/day-01/Datatypes/DMA/01.cpp/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
-
 using namespace std;
-
 // Please allocate a 5 long array and fill it with numbers from 0 to 4, then print the whole array
 // Please delete the array before the program exits
+int main()
+ {
+    int* pointer = NULL;
+    int lenght = 5;
+    pointer = new int[lenght];
 
-int main() {
-  int* pointer = NULL;
+    for (int i = 0; i < lenght; ++i) {
+        pointer[i] = i;
+    }
 
+    for (int i = 0; i < lenght; ++i) {
+        cout << pointer[i] << ", ";
+    }
 
-  return 0;
+    delete []pointer;
+
+    return 0;
 }
