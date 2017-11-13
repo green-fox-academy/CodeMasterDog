7460df0d513aad2bda6d06a4daea55c3868d76f5
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-13 16:00:51 +0100

    Datatypes/Vector/02.cpp task done

--- week-06/day-01/Datatypes/Vector/02.cpp/main.cpp
+++ week-06/day-01/Datatypes/Vector/02.cpp/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
 int main() {
 	//Create a vector of integers
 	//reserve place for 10 element, then fill those place with random numbers between 0 and 10
+    vector <int> int_vector(10);
 
+    srand(time(NULL));
+
+    for (int i = 0; i < int_vector.size(); ++i) {
+        int_vector.at(i) = (rand() % 100 + 1);
+        cout << int_vector[i] << " ,";
+    }
   return 0;
 }
