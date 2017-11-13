713aad0e578e16ea4c3268e46da0c4516f3346c2
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-13 14:46:27 +0100

    Datatypes/Namespace/04.cpp task started

---
+++ week-06/day-01/Datatypes/Namespace/04.cpp/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Create your own namespace, called Mark
+// store your name as a string
+// your age as an integer
+// and your favorite number as an integer
+//
+// Create your own namespace, called Maria
+// store your name as a string
+// your age as an integer
+// and your favorite number as an integer
+//
+// create a function that can add the two age, and returns it
+
+int main()
+{
+
+    return 0;
+}
