84d94f479c1a7e26e414e0e801fc279db3d1ceb9
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-13 15:33:01 +0100

    Datatypes/Vector/01.cpp task done

--- week-06/day-01/Datatypes/Vector/01.cpp/main.cpp
+++ week-06/day-01/Datatypes/Vector/01.cpp/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+//create an integer vector with the size of 5 and print all elements of it
 using namespace std;
 
-int main() {
-	//create an integer vector with the size of 5 and print all elements of it
+int main()
+{
+    vector<int> my_vector;
 
-  return 0;
+    my_vector.push_back(77);
+    my_vector.push_back(69);
+    my_vector.push_back(5);
+    my_vector.push_back(4);
+    my_vector.push_back(6);
+
+    for (int i = 0; i < my_vector.size(); ++i) {
+        cout <<  my_vector[i] << ", ";
+    }
+
+    return 0;
 }
