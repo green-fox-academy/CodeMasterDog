501793ee11ebee56fbbe1aa052af5334048fceaf
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-13 15:15:55 +0100

    Datatypes/Vector/00.cpp task done

--- week-06/day-01/Datatypes/Vector/00.cpp/main.cpp
+++ week-06/day-01/Datatypes/Vector/00.cpp/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
-
 //create an integer vector with the size of 5 and print the 3rd element of it
+int main()
+{
+    vector<int> my_vector = {77, 69, 5, 4, 6};
 
-int main() {
-
+    cout << "The vector's 3rd element is: "<<  my_vector[2] ;
 
   return 0;
 }
