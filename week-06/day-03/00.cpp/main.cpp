de910c29e834352a9123759c732180c40852e2bf
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-15 11:43:12 +0100

    oo-part2/Workshop/00.cpp task done

--- week-06/day-03/00.cpp/main.cpp
+++ week-06/day-03/00.cpp/main.cpp
@@ -1,15 +1,50 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 // Create a Human class which is a base class for
 // SuperHuman and AquaHuman
 // Create a virtual function in Human class called travelling()
 // It should print out different ways to travel
 // For example: Human is walking. SuperHuman is flying. AquaHuman is riding with dolphins.
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Human {
+    public:
+        void virtual travelling()
+        {
+            cout << "I can walk." << endl;
+        }
+};
+
+class SuperHuman : public Human {
+    public:
+        void travelling()
+        {
+            cout << "I can fly." << endl;
+        }
+};
+
+class AquaHuman : public Human {
+    public:
+        void travelling()
+        {
+            cout << "I can riding with dolphins." << endl;
+        }
+};
 
 int main()
 {
+    Human h;
+    cout << "Human says: ";
+    h.travelling();
+
+    cout << "SuperHuman says: ";
+    SuperHuman sh;
+    sh.travelling();
+
+    cout << "AquaHuman says: ";
+    AquaHuman ah;
+    ah.travelling();
+
     return 0;
 }
