0f589aaeb4db566ccb9b5541d402add2ec7a18c6
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-20 14:40:45 +0100

    Exceptions 05 task done

--- week-07/day 01/05.cpp/main.cpp
+++ week-07/day 01/05.cpp/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
+// Create a function which throws an char
+// In the main(), put that function in a try block
+// Also in the main() catch, what your function throws
+
+float divide(int, int) throw (char);
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    try
+    {
+        int a = 20;
+        int b = 0;
+
+        cout << divide(a, b) << endl;
+    }
+    catch (char x)
+    {
+        cout << "Error occurred, caught a char: '" << x << "'" << endl;
+    }
+
+	return 0;
+}
+
+
+float divide(int a, int b) throw (char)
+{
+    if (b == 0)
+        throw 'c';
+
+    return a / b;
 }
