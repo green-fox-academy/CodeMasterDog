a178bb2e22c75920f043da95defca3aee733d317
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-20 23:07:20 +0100

    Exceptions 10 task start

---
+++ week-07/day 01/10.cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Create a function that prints out the fist "N" positive odd numbers.
+ * It should take the "N" number as a parameter, and print the output to the console.
+ *
+ * The output should look like this for 5 as parameter:
+ *   1, 3, 5, 7, 9
+ *
+ * Non-positive "N" numbers should be treated as errors, handle it!
+ */
+
+int main() {
+
+  return 0;
+}
