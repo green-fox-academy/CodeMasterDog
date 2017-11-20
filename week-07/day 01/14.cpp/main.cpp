b003b703b4c478932aedf13822a8ed051fe4851f
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-21 00:31:18 +0100

    Exceptions 14 task started

---
+++ week-07/day 01/14.cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+// Create a class named Person.
+// This class shoud have a constructor which prints out the
+// date of the creation of the istance.
+
+int main()
+{
+
+
+}
