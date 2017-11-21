bc967be9e66440e5ab5874d32615384cc3c2e950
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-21 15:24:09 +0100

    Overloading Functions & Methods 02 task started

---
+++ week-07/day-02/02.cpp/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+// Create 3 functions: each should take respectively 2, 3 and 4 strings.
+// Each should return a string that contains all the input string concatenated.
+// (Combined into one in a linear order.)
+// Write code that showcases how this works and prints out the result of the function.
+
+int main()
+{
+
+
+	return 0;
+}
