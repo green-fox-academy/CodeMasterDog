985bf3b75d5b1e86b8d1d243ab0dab2d2a05cf83
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-21 16:55:43 +0100

    Overloading Functions & Methods 06 task started

---
+++ week-07/day-02/06.cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// Take the Integer class from the previous exercise and add two methods:
+// One which takes an Integer& parameter and adds the value of it to the member variable of this.
+// One which takes an int and adds the value of it to the member variable of this.
+//
+// Write code which uses them both and Prints out the results respectively.
+
+int main()
+{
+
+
+	return 0;
+}
