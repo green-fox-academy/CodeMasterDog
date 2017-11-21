c0dc8d5468c81c121780531092ef2de50ad20c7d
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-21 15:59:38 +0100

    Overloading Functions & Methods 03 task done

--- week-07/day-02/03.cpp/main.cpp
+++ week-07/day-02/03.cpp/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 using namespace std;
+
+float FunctionCalledTheSame(float, float);
+int FunctionCalledTheSame(int, int);
 // Now, create 2 functions. They should bear the same name.
 // These functions should multiple their parameters.
 // The first should return float and take two arguments of the type float.
@@ -11,13 +14,31 @@ using namespace std;
 // Now, put one of the functions into comment. (So it's still legible but won't compile.)
 // Try compiling it again, and watch what the results are.
 //
-// Switch the two function declarations. Ucomment the one You commented out and comment out the one
+// Switch the two function declarations. Comment the one You commented out and comment out the one
 // that ran before. Compile it and compare the results again.
 //
 // What did You learn from this exercise?
 // Write it down in a comment into Your file!
 int main()
 {
+    float a = 3.66;
+    float b = 8.16;
+    int c = 3;
+    int d = 8;
+
+    cout << "a * b = " << FunctionCalledTheSame(a, b) << endl;
+    cout << "c * d = " << FunctionCalledTheSame(c, d) << endl;
 
     return 0;
 }
+
+float FunctionCalledTheSame(float var_a, float var_b)
+{
+    return var_a * var_b;
+}
+
+int FunctionCalledTheSame(int var_a, int var_b)
+{
+    return var_a * var_b;
+}
+
