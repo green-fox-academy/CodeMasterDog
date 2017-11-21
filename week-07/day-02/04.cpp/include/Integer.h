a43337b05e0358282c2e496b89b7a7aff6e77e8f
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-21 16:02:50 +0100

    Overloading Functions & Methods 04 task class creeated

---
+++ week-07/day-02/04.cpp/include/Integer.h
@@ -0,0 +1,16 @@
+#ifndef INTEGER_H
+#define INTEGER_H
+
+
+class Integer
+{
+    public:
+        Integer();
+        virtual ~Integer();
+
+    protected:
+
+    private:
+};
+
+#endif // INTEGER_H
--- week-07/day-02/04.cpp/src/Integer.cpp
+++ week-07/day-02/04.cpp/src/Integer.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
 #include "Integer.h"
 
-using namespace std;
 // Create a Class named Integer. It should have one int member variable.
 // Create add() methods to it. They should take Integer& parameters.
 // Make variations of add() method 1, 2, 3, 4 and 5 arguments respectively.
 // All methods should add the value of the parameters to the member variable of this.
 //
 // Try them out. Write code that demonstrates how they work.
-int main()
+Integer::Integer()
 {
+    //ctor
+}
 
-
-    return 0;
+Integer::~Integer()
+{
+    //dtor
 }
