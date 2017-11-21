6cd45c64d881d34bfff006eebd0fd0a93817305a
Laszlo Kovacs <kovacslaa@gmail.com>
2017-11-21 16:20:14 +0100

    Overloading Functions & Methods 05 task started

---
+++ week-07/day-02/05.cpp/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+// Take the Integer class from the previous exercise.
+// Create 3 constructors for them:
+// One empty which sets the member varibale to 0;
+// A copy constructor, that takes an Integer& argument
+// A third one which takes an int as argument and makes the member variable equal to the int it took.
+//
+// Write code that uses these and prints out the value of the member variable of the integers created.
+// (You will have to create a get method to do this appropriately.)
+
+
+int main()
+{
+
+
+	return 0;
+}
