d057e1b697ca6081040b09cd2d0c825983c88ae1
Laszlo Kovacs <kovacslaa@gmail.com>
2018-03-05 15:41:40 +0100

    minor changes in the RPG, just for to 'back in the game' again

--- week-10/Practice/05/src/Character.cpp
+++ week-10/Practice/05/src/Character.cpp
@@ -2,12 +2,12 @@
 
 Character::Character(bool character_type)
 {   //player
-    if (character_type) {
+    if (character_type) {   //player
         health = 100;
         attack = 15;
         defense = 30;
     }
-    else {
+    else {                  //monster
         health = 150;
         attack = 26;
         defense = 9;
@@ -23,12 +23,12 @@ void Character::getter_other()
 {
     cout << "attack value: " << attack << endl;
     cout << "defense value: " << defense << endl;
-    cout << "character type: " << character_type << endl;
+    cout << "character type: " << std::boolalpha << character_type << endl;
 }
 //attacking method which takes a character class and decreases its health
 //value with (current character class attack - input character class defense),
 //if the output is minus, then our health is decreased
-//set the attacking method to have 50% chanse to count our attack value twice,
+//set the attacking method to have 50% chance to count our attack value twice,
 //(2xcurrent attack - input defense) before calculating the final health points.
 
 void Character::attacking(Character& enemy)
